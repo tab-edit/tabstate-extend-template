4eef542466d814d326c9e7d178360e477ff76694
Stanley Ihesiulo <stanihe1901@gmail.com>
2023-08-02 16:26:38 -0400

    modified template to emphasize overriding rules

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,16 @@ import ruleMap from './rules/index';
 export default {
     RuleSource: ruleMap,
     Plugin: {
-        extend: 'some-plugin-name',     // the plugin you extend here should be trying to achieve the same behaviour. In fact, its plugin value must be of the same shape as your plugin value.
-        compute: ['rule-2', 'rule-3', 'my-custom-rule'],
+        extend: 'some-similar-plugin',     // the plugin you extend here should be trying to achieve the same behaviour. In fact, its plugin value must be of the same shape as your plugin value.
+        compute: ['rule-c', 'rule-d', 'my-custom-rule'],
         configure: {
-            'rule-2': 'value',
+            'rule-c': 'value',
             'my-custom-rule': { property: 'value' }
         },
         override: {                     // override rules used in ancestor plugins
-            'rule-1': 'new-rule-1',
-            'rule-0': {
-                rule: 'new-rule-0',
+            'rule-a': 'new-rule-a',
+            'rule-b': {
+                rule: 'new-rule-b',
                 config: { property: 3 }
             }
         }
--- src/rules/index.ts
+++ src/rules/index.ts
@@ -1,3 +1,5 @@
 export default {
-    "my-custom-rule": () => import("./my-custom-rule")
+    "my-custom-rule": () => import("./my-custom-rule"),
+    "new-rule-a": () => import("./new-rule-a"),
+    "new-rule-b": () => import("./new-rule-b"),
 }
