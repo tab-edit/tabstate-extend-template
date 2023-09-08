44873b786c7cc5cc875f569f5c3cd2007ee16425
Stanley Ihesiulo <stanihe1901@gmail.com>
2023-09-07 22:53:36 -0400

    migrated from webpack to rollup

---
+++ rollup.config.js
@@ -0,0 +1,18 @@
+import typescript from "@rollup/plugin-typescript";
+import del from 'rollup-plugin-delete';
+
+export default [
+    {
+        input: 'src/index.ts',
+        output: [
+            {
+                dir: 'dist',
+                format: 'cjs',
+            },
+        ],
+        plugins: [
+            typescript(),
+            del({ targets: 'dist/*' })
+        ]
+    }
+]
