2ccc271824a50342691eed8eebbc67066200a79c
Stanley Ihesiulo <stanihe1901@gmail.com>
2023-09-07 22:57:06 -0400

    changed from cjs to es

--- rollup.config.js
+++ rollup.config.js
@@ -7,7 +7,7 @@ export default [
         output: [
             {
                 dir: 'dist',
-                format: 'cjs',
+                format: 'es',
             },
         ],
         plugins: [
